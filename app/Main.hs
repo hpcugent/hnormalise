f477f19dc75a19137c2f95890fc1d6bddc3ab661
Andy Georges <itkovian@gmail.com>
2017-05-03 00:59:07 +0200

    Output JSON for torque example

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,18 @@
 
 module Main where
 
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
 import Data.Attoparsec.Text
+import qualified Data.ByteString.Lazy.Char8 as BS
 import Torque.Parser
+import Torque.Json
 
 exitTestLine = "04/05/2017 13:06:53;E;45.master23.banette.gent.vsc;user=vsc40075 group=vsc40075 jobname=STDIN queue=short ctime=1491390300 qtime=1491390300 etime=1491390300 start=1491390307 owner=vsc40075@gligar01.gligar.gent.vsc exec_host=node2801.banette.gent.vsc/0-1+node2803.banette.gent.vsc/0-1 Resource_List.nodes=node2801.banette.gent.vsc:ppn=2+node2803.banette.gent.vsc:ppn=2 Resource_List.vmem=1gb Resource_List.nodect=2 Resource_List.neednodes=node2801.banette.gent.vsc:ppn=2+node2803.banette.gent.vsc:ppn=2 Resource_List.nice=0 Resource_List.walltime=01:00:00 session=15273 total_execution_slots=4 unique_node_count=2 end=1491390413 Exit_status=0 resources_used.cput=0 resources_used.energy_used=0 resources_used.mem=55048kb resources_used.vmem=92488kb resources_used.walltime=00:01:44\n"
 
 main :: IO ()
 main = do
-    parseTest parseTorqueExit exitTestLine
+    let t = parse parseTorqueExit exitTestLine
+    case t of
+        Done _ te -> BS.putStrLn $ encodePretty te
+        _ -> putStrLn "Failed"

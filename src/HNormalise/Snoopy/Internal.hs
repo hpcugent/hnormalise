53a097712525ce70a724cb5d2f5a747d7f6d6676
Andy Georges <itkovian@gmail.com>
2017-05-27 00:17:37 +0200

    Snoopy parser

--- src/HNormalise/Snoopy/Internal.hs
+++ src/HNormalise/Snoopy/Internal.hs
@@ -14,3 +14,14 @@ import           GHC.Generics               (Generic)
 
 --------------------------------------------------------------------------------
 import           HNormalise.Common.Internal
+-- snoopy[27316]::  [uid:110 sid:9379 tty:(none) cwd:/ filename:/usr/lib64/nagios/plugins/hpc/check_ifutil.pl]: /usr/lib64/nagios/plugins/hpc/check_ifutil.pl -i em1.295 -w 90 -c 95 -p -b 10000m
+data Snoopy = Snoopy
+    { pid        :: !Int
+    , uid        :: !Int
+    , username   :: !(Maybe Text)
+    , sid        :: !Int
+    , tty        :: !Text
+    , cwd        :: !Text
+    , filename   :: !Text
+    , command    :: !Text
+    } deriving (Show, Eq, Generic)
--- src/HNormalise/Snoopy/Parser.hs
+++ src/HNormalise/Snoopy/Parser.hs
@@ -9,6 +9,7 @@ module HNormalise.Snoopy.Parser where
 import           Control.Applicative        ((<|>))
 import           Data.Attoparsec.Combinator (lookAhead, manyTill)
 import           Data.Attoparsec.Text
+import           Data.Char                  (isSpace)
 import qualified Data.Text                  as T
 
 --------------------------------------------------------------------------------
@@ -16,3 +17,27 @@ import           HNormalise.Common.Parser
 import           HNormalise.Snoopy.Internal
 
 --------------------------------------------------------------------------------
+-- snoopy[27316]::  [uid:110 sid:9379 tty:(none) cwd:/ filename:/usr/lib64/nagios/plugins/hpc/check_ifutil.pl]: /usr/lib64/nagios/plugins/hpc/check_ifutil.pl -i em1.295 -w 90 -c 95 -p -b 10000m
+parseSnoopy :: Parser Snoopy
+parseSnoopy = do
+    string "snoopy["
+    pid <- decimal
+    takeTill ( == '[') *> char '['
+    uid <- string "uid:" *> decimal
+    username <- maybeOption $ string "username:" *> takeTill isSpace
+    sid <- skipSpace *> string "sid:" *> decimal
+    tty <- skipSpace *> string "tty:" *> takeTill isSpace
+    cwd <- skipSpace *> string "cwd:" *> takeTill isSpace
+    filename <- skipSpace *> "filename:" *> takeTill ( == ']')
+    string "]:"
+    command <- skipSpace *> takeText
+    return $ Snoopy
+        { pid = pid
+        , uid = uid
+        , username = username
+        , sid = sid
+        , tty = tty
+        , cwd = cwd
+        , filename = filename
+        , command = command
+        }
